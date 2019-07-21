905e07da7d098591ff31cf7dcfa610a3627c3a81
Others <Others@Action>
2019-07-21 13:25:37 +0530

    test class

---
+++ Concept/src/JavaTest/Test.java
@@ -0,0 +1,15 @@
+package JavaTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Printing system date and time
+		
+		System.out.println("current tyme of the cyctem is ==> "+ System.currentTimeMillis());
+	
+		
+		
+		
+	}
+
+}
